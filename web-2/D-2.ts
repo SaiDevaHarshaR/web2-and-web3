7b643f01fdf002fc0388a6860df050b1bdafa873
SaiDevaHarshaR <saidevapro@gmail.com>
2025-09-17 19:04:46 +0530

    Added D-2.js file

---
+++ web-2/D-2.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+export const app = express()
+app.use(express.json());
+app.post("/sum", (req, res) => {
+
+    const a = req.body.a;
+    const  b = req.body.b;
+
+    const result = a + b;
+
+    res.json({sum: result});
+
+});
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
+
+
+```
+the test file as follows:
+
+import { describe, it} from '@jest/globals';
+import {app} from './D-2';
+
+describe("test the sum function", () => {
+it("should return 3 when 1+2", () => {
+axios.post("http://localhost:3000/sum", {a:1, b:2}).then((res) => {
+expect(res.data.sum).toBe(3);
+})
+
+})
+
+```
